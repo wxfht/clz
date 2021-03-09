3472245b7d900471f9ae3007256f8f3145a6959e
hutong <123456789@qq.com>
2021-03-09 10:09:32 +0800

    首次提交

---
+++ notify.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * lionfish 商城系统
+ *
+ * ==========================================================================
+ * @link      http://www.liofis.com/
+ * @copyright Copyright (c) 2015-2016 liofis.com. 
+ * @license   http://www.liofis.com/license.html License
+ * ==========================================================================
+ *
+ * @author    fish
+ *
+ */
+ 
+if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+
+header("Content-Type:text/html; charset=utf-8");    
+
+define('APP_DEBUG', true);
+
+define('BIND_MODULE','Home');
+  
+define ('APP_PATH', './Modules/' );
+
+
+if (!is_file( 'Modules/Common/Conf/db.php')) {
+    header('Location: ./install.php');
+    exit;
+}
+//index.php?c=Public&a=login
+//Payment/weixin_notify
+$_GET['c'] = 'Payment';
+$_GET['a'] = 'weixin_notify';
+define('ROOT_PATH',str_replace('\\','/',dirname(__FILE__)) . '/'); 
+
+define ('RUNTIME_PATH','./Runtime/');
+
+require './ThinkPHP/ThinkPHP.php';
+
+?>
